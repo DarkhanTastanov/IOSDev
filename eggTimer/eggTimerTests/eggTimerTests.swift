e0b2a2811c8ce2a9d2c36404ce58d377b7a4a11d
DarkhanTastanov <153364440+DarkhanTastanov@users.noreply.github.com>
2024-10-11 12:59:46 +0500

    Add files via upload

---
+++ eggTimer/eggTimerTests/eggTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  eggTimerTests.swift
+//  eggTimerTests
+//
+//  Created by Student on 11.10.2024.
+//
+
+import Testing
+@testable import eggTimer
+
+struct eggTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
